b23fc5f17e3021b513fb8c97a8b70c66d0b613e3
diddyblink <11davidegarbe@gmail.com>
2025-05-20 17:29:16 +0200

    Aggiunte estensioni VSCodium

--- configuration.nix
+++ configuration.nix
@@ -81,6 +81,7 @@ home = "/home/diddy";
    programs.firefox.enable = true;
 
 
+   nixpkgs.config.allowUnfree = true; 
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -104,7 +105,24 @@ home = "/home/diddy";
      neofetch
      podman
      opentofu
-     vscodium 
+     vscodium
+(vscode-with-extensions.override {
+    vscode = vscodium;
+    vscodeExtensions = with vscode-extensions; [
+      bbenoist.nix
+      ms-python.python
+      ms-azuretools.vscode-docker
+      ms-vscode-remote.remote-ssh
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "remote-ssh-edit";
+        publisher = "ms-vscode-remote";
+        version = "0.47.2";
+        sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+      }
+    ];
+  })
+ 
    ];
 
 virtualisation.podman.enable = true;
