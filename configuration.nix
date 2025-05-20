3e7510ca6802c1ff80a387f67fb12ab4ce611cb2
diddyblink <11davidegarbe@gmail.com>
2025-05-20 16:56:44 +0200

    Rimosse estensioni VSCodium ma aggiunto VSCodium

--- configuration.nix
+++ configuration.nix
@@ -103,23 +103,8 @@ home = "/home/diddy";
      dunst
      neofetch
      podman
-     opentofu 
-     (vscode-with-extensions.override {
-    vscode = vscodium;
-    vscodeExtensions = with vscode-extensions; [
-      bbenoist.nix
-      ms-python.python
-      ms-azuretools.vscode-docker
-      ms-vscode-remote.remote-ssh
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "remote-ssh-edit";
-        publisher = "ms-vscode-remote";
-        version = "0.47.2";
-        sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-      }
-    ];
-  })
+     opentofu
+     vscodium 
    ];
 
 virtualisation.podman.enable = true;
