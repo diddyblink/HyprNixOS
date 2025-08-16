22306a63d2369d17ed94bce2792ea858c5018f10
diddyblink <11davidegarbe@gmail.com>
2025-08-16 19:00:54 +0200

    fix: set Home Manager username to diddy

--- home/diddy/home.nix
+++ home/diddy/home.nix
@@ -1,39 +1,22 @@
 { config, pkgs, ... }:
-
 {
-  home.username = "<username>";
-  home.homeDirectory = "/home/<username>";
+  home.username = "diddy";
+  home.homeDirectory = "/home/diddy";
+
   programs.home-manager.enable = true;
 
-  # Pacchetti utente
   home.packages = with pkgs; [
-    hyprland
-    hyprpaper
-    hyprlock
-    waybar
-    kitty
-    git
-    scid  # vedi §3
-    # scid_vs_pc  # opzionale, GUI alternativa
+    hyprland hyprpaper hyprlock waybar kitty
+    scid
   ];
 
-  # Symlink gestiti da Home Manager verso i file nel repo
   xdg.configFile."hypr/hyprland.conf".source = ./hypr/hyprland.conf;
   xdg.configFile."hypr/hyprlock.conf".source = ./hypr/hyprlock.conf;
-  xdg.configFile."waybar/config.jsonc".text = ''
-    {
-      // esempio minimale
-      "layer": "top",
-      "modules-right": ["clock"]
-    }
-  '';
 
-  # Esempio: variabili utili
   home.sessionVariables = {
     XDG_CURRENT_DESKTOP = "Hyprland";
     NIXOS_OZONE_WL = "1";
   };
 
-  # Versione Home Manager (per riproducibilità)
-  home.stateVersion = "24.05";
+  home.stateVersion = "24.11";
 }
